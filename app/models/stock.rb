1d4c0570874e5f5c725a9192287c1b382cd7e71d
Fabio Urquiza <flus@cesar.org.br>
2020-01-29 15:49:19 -0300

    Add stock model and iex API key

---
+++ app/models/stock.rb
@@ -0,0 +1,10 @@
+class Stock < ApplicationRecord
+
+  def self.new_lookup(ticket_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+    client.price(ticket_symbol);
+  end
+end
