a9abe81c108ed5525fc05971975b088bc606cb73
Fabio Urquiza <flus@cesar.org.br>
2020-01-30 16:49:43 -0300

    Add delete button to remove stock from user portifolio

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -7,4 +7,11 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.ticket} was successfully added to your portifolio"
     redirect_to portifolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    UserStock.destroy_by(user_id: current_user.id, stock_id: params[:id])
+    flash[:notice] = "Stock #{stock.ticket} was successfully removed from your portifolio"
+    redirect_to portifolio_path
+  end
 end
